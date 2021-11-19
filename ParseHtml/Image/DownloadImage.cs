db678a3bffb205a0fe4938a2cc3be97299fe8c91
Alexey <alesha.kolpashikov@gmail.com>
2021-11-20 01:42:56 +0300

    finish download images

--- ParseHtml/Image/DownloadImage.cs
+++ ParseHtml/Image/DownloadImage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,8 +16,18 @@ namespace ParseHtml.Image
             FilePath = Directory.Exists(filePath) ? filePath : "C:";
         }
 
-        public void Download(string searchQuery)
+        public void Download(string searchQuery,int count)
         {
+            ParserImage parser = new ParserImage();
+            var resultList = parser.Parse(searchQuery, count);
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36");
+            foreach (var res in resultList)
+            {
+                Uri uri = new Uri(res);
+                client.DownloadFile(uri, FilePath + res.Split('/').Last());
+            }
 
         }
     }

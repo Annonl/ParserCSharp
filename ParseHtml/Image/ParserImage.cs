6dc7364e0270727cdfafc5aea8ec4d038ff8b2a2
Alexey <alesha.kolpashikov@gmail.com>
2021-11-19 20:27:23 +0300

    write parse image

--- ParseHtml/Image/ParserImage.cs
+++ ParseHtml/Image/ParserImage.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ParseHtml.Image
 {
-    class ParserImage
+    public class ParserImage
     {
+        public List<string> Parse(string searchQuery)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/imghp?hl=ru&ogbl");
+
+            IWebElement element = driver.FindElement(By.ClassName("gLFyf gsfi"));
+            element.SendKeys(searchQuery);
+
+            // Get the search results panel that contains the link for each result.
+            IWebElement resultsPanel = driver.FindElement(By.ClassName("Tg7LZd"));
+
+            ReadOnlyCollection<IWebElement> searchResults = resultsPanel.FindElements(By.ClassName("wXeWr islib nfEiy"));
+            var result = new List<string>();
+            foreach (IWebElement searchResult in searchResults)
+            {
+                result.Add(searchResult.GetAttribute("href"));
+            }
+            return result;
+        }
     }
 }
